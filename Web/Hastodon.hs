ed75fe3eee3a97b563659418e661f8959b74d49f
Ryo Okubo <syucream1031@gmail.com>
2017-04-21 00:29:52 +0900

    Implement postStatuses

--- Web/Hastodon.hs
+++ Web/Hastodon.hs
@@ -3,6 +3,7 @@ module Web.Hastodon
     mkHastodonClient
 
   , getAccountsById
+  , postStatuses
   , getHomeTimeline
   , getPublicTimeline
   ) where
@@ -12,23 +13,42 @@ import Data.ByteString.Lazy.Char8 as LChar8
 import Network.HTTP.Simple
 import Network.HTTP.Types.Header
 
+--
 -- Mastodon API endpoints
-uAccountsById = "/api/v1/accounts/" -- :id
-uHomeTimeline = "/api/v1/timelines/home"
-uPublicTimeline = "/api/v1/timelines/public"
+--
+pAccountsById   = "/api/v1/accounts/" -- :id
+pStatuses       = "/api/v1/statuses"
+pHomeTimeline   = "/api/v1/timelines/home"
+pPublicTimeline = "/api/v1/timelines/public"
 
 data HastodonClient = HastodonClient {
   host :: String,
   token :: String
 }
 
+-- 
+-- helpers
+-- 
+
 mkHastodonHeader :: String -> Request -> Request
 mkHastodonHeader token =
   addRequestHeader hAuthorization $ Char8.pack $ "Bearer " ++ token
 
-getHastodonResponseBody url client = do
-  initReq <- parseRequest $ "https://" ++ (host client) ++ url
-  let req = mkHastodonHeader (token client) initReq
+mkHastodonRequest :: String -> HastodonClient -> IO Request
+mkHastodonRequest path client = do
+  initReq <- parseRequest $ "https://" ++ (host client) ++ path
+  return $ mkHastodonHeader (token client) $ initReq
+
+getHastodonResponseBody :: String -> HastodonClient -> IO String
+getHastodonResponseBody path client = do
+  req <- mkHastodonRequest path client
+  res <- httpLBS req
+  return $ LChar8.unpack $ getResponseBody res
+
+postHastodonRequestBody :: String -> [(Char8.ByteString, Char8.ByteString)] -> HastodonClient -> IO String
+postHastodonRequestBody path body client = do
+  initReq <- mkHastodonRequest path client
+  let req = setRequestBodyURLEncoded body $ initReq
   res <- httpLBS req
   return $ LChar8.unpack $ getResponseBody res
 
@@ -40,11 +60,14 @@ mkHastodonClient :: String -> String -> HastodonClient
 mkHastodonClient host token = HastodonClient host token
 
 getAccountsById :: String -> HastodonClient -> IO String
-getAccountsById id = getHastodonResponseBody $ uAccountsById ++ id
+getAccountsById id = getHastodonResponseBody $ pAccountsById ++ id
+
+postStatuses :: String -> HastodonClient -> IO String
+postStatuses statuses = postHastodonRequestBody pStatuses [(Char8.pack "status", Char8.pack statuses)]
 
 getHomeTimeline :: HastodonClient -> IO String
-getHomeTimeline = getHastodonResponseBody uHomeTimeline
+getHomeTimeline = getHastodonResponseBody pHomeTimeline
 
 getPublicTimeline :: HastodonClient -> IO String
-getPublicTimeline = getHastodonResponseBody uPublicTimeline
+getPublicTimeline = getHastodonResponseBody pPublicTimeline
 
