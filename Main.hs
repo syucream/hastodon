18f10584a8b0d1ef04a035030f60d728c55d1291
as_capabl <as-capabl@gmail.com>
2018-05-08 00:06:52 +0900

    Option support

--- Main.hs
+++ Main.hs
@@ -1,4 +1,6 @@
 import Web.Hastodon
+import Data.Monoid ((<>))
+import Control.Concurrent (threadDelay)
 
 --
 -- example main()
@@ -12,7 +14,22 @@ main = do
   maybeClient <- mkHastodonClient clientId clientSecret username password "mastodon.social"
   case maybeClient of
     Just client -> do
-      timeline <- getAccountById client 93150 
-      print timeline
-      result <- postStatus client "test toot from hastodon!" 
+      let accId = 93150
+
+      account <- getAccountById client accId
+      print account
+
+      timelineE <- getAccountStatusesWithOption client (limit 5) accId
+      timelime <- either (fail . show) return timelineE
+      print $ take 10 . statusContent <$> timelime
+
+      postedE <- postStatus client "test toot from hastodon!"
+      posted <- either (fail . show) return postedE
+      print posted
+
+      threadDelay 50000
+      let stId = read (statusId posted)
+      result <- postStatusWithOption client
+          (inReplyToId stId <> visibility VisibilityPrivate)
+          "test reply"
       print result
